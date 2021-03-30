a4d37edd25890d2f7c1198c23b37a9e26d4adb73
aegroto <aegroto@protonmail.com>
2021-03-30 17:16:39 +0200

    nal-based clone script

--- src/main.rs
+++ src/main.rs
@@ -8,41 +8,26 @@ use std::io::Read;
 use h264_reader::annexb::AnnexBReader;
 use h264_reader::annexb::NalReader;
 use h264_reader::Context;
-use h264_reader::rbsp::RbspDecoder;
-use h264_reader::nal::NalHandler;
-use h264_reader::nal::NalHeader;
 
-struct NullNalHandler {
-}
-impl NalHandler for NullNalHandler {
-    type Ctx = ();
-
-    fn start(&mut self, _ctx: &mut Context<Self::Ctx>, _header: NalHeader) {
-        unimplemented!()
-    }
-
-    fn push(&mut self, _ctx: &mut Context<Self::Ctx>, _buf: &[u8]) {
-        unimplemented!()
-    }
-
-    fn end(&mut self, _ctx: &mut Context<Self::Ctx>) {
-        unimplemented!()
-    }
-}
-
-struct NullNalReader {
-    decoder: RbspDecoder<NullNalHandler>,
+struct EnhanceNalReader {
+    output_file: File,
     start: u64,
     push: u64,
     end: u64,
 }
-impl NalReader for NullNalReader {
+
+impl NalReader for EnhanceNalReader {
     type Ctx = ();
 
     fn start(&mut self, _ctx: &mut Context<Self::Ctx>) {
         self.start += 1;
     }
-    fn push(&mut self, _ctx: &mut Context<Self::Ctx>, _buf: &[u8]) {
+
+    fn push(&mut self, _ctx: &mut Context<Self::Ctx>, buf: &[u8]) {
+        self.output_file.write(&[0x00, 0x00, 0x01]).unwrap();
+
+        self.output_file.write_all(&*buf).unwrap(); 
+
         self.push += 1;
     }
     fn end(&mut self, _ctx: &mut Context<Self::Ctx>) {
@@ -54,16 +39,15 @@ fn main() -> Result<(), Error> {
     let args: Vec<String> = env::args().collect();
 
     let mut input_file = File::open(&args[1]).unwrap();
-    let mut output_file = File::create(&args[2]).expect("Unable to create file");   
+    let output_file = File::create(&args[2]).expect("Unable to create file");   
 
     let size = input_file.metadata()?.len() as usize;
 
     let mut buf = vec![0; size];
     input_file.read(&mut buf[..]).unwrap();
     let mut ctx = Context::default();
-    let nal_handler = NullNalHandler {};
-    let nal_reader = NullNalReader {
-        decoder: RbspDecoder::new(nal_handler),
+    let nal_reader = EnhanceNalReader {
+        output_file: output_file,
         start: 0,
         push: 0,
         end: 0,
@@ -74,5 +58,5 @@ fn main() -> Result<(), Error> {
     annexb_reader.push(&mut ctx, &buf[..]);
     annexb_reader.end_units(&mut ctx);
 
-    output_file.write_all(&*buf)
+    Ok(())
 }
