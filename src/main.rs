0fffafab8ad4248012a36dd5ae90f47a5d25ba5b
aegroto <aegroto@protonmail.com>
2021-03-30 16:25:41 +0200

    simple h264 copy

--- src/main.rs
+++ src/main.rs
@@ -1,82 +1,78 @@
-use std::io::BufWriter;
 use std::env;
 
+use std::io::Error;
+use std::io::Write;
 use std::fs::File;
-use mp4::{Result, Mp4Config, Mp4Reader, Mp4Track, Mp4Writer, TrackConfig, Vp9Config, MediaType};
-use mp4::{MediaConfig, HevcConfig, AvcConfig, AacConfig, TtxtConfig};
-use std::io::{BufReader};
 
-fn main() -> Result<()> {
-    let args: Vec<String> = env::args().collect();
+use std::io::Read;
+use h264_reader::annexb::AnnexBReader;
+use h264_reader::annexb::NalReader;
+use h264_reader::Context;
+use h264_reader::rbsp::RbspDecoder;
+use h264_reader::nal::NalHandler;
+use h264_reader::nal::NalHeader;
 
-    let input_file = File::open(&args[1]).unwrap();
-    let output_file = File::create(&args[2]).expect("Unable to create file");                                                                                                          
+struct NullNalHandler {
+}
+impl NalHandler for NullNalHandler {
+    type Ctx = ();
 
-    let size = input_file.metadata()?.len();
-
-    let reader = BufReader::new(input_file);
-    let writer = BufWriter::new(output_file);
-
-    let mut mp4_reader = Mp4Reader::read_header(reader, size)?;
-    let mut mp4_writer = Mp4Writer::write_start(
-        writer,
-        &Mp4Config {
-            major_brand: mp4_reader.major_brand().clone(),
-            minor_version: mp4_reader.minor_version(),
-            compatible_brands: mp4_reader.compatible_brands().to_vec(),
-            timescale: mp4_reader.timescale(),
-        },
-    )?;
-
-    for track_idx in 0..mp4_reader.tracks().len() {
-        let track = mp4_reader.tracks().get(track_idx).unwrap();
-        let track_conf = TrackConfig {
-            track_type: track.track_type()?,
-            timescale: track.timescale(),
-            language: track.language().to_string(),
-            media_conf: extract_media_conf(track)?,
-        };
-
-        mp4_writer.add_track(&track_conf)?;
-
-        let track_id = track_idx as u32 + 1;
-        let sample_count = mp4_reader.sample_count(track_id)?;
-        for sample_idx in 0..sample_count {
-            let sample_id = sample_idx + 1;
-            let sample = mp4_reader.read_sample(track_id, sample_id)?.unwrap();
-            mp4_writer.write_sample(track_id, &sample)?;
-        }
+    fn start(&mut self, _ctx: &mut Context<Self::Ctx>, _header: NalHeader) {
+        unimplemented!()
     }
 
-    mp4_writer.write_end()?;
+    fn push(&mut self, _ctx: &mut Context<Self::Ctx>, _buf: &[u8]) {
+        unimplemented!()
+    }
 
-    Ok(())
+    fn end(&mut self, _ctx: &mut Context<Self::Ctx>) {
+        unimplemented!()
+    }
 }
 
-fn extract_media_conf(track: &Mp4Track) -> Result<MediaConfig> {
-    let media_type = match track.media_type()? {
-        MediaType::H264 => MediaConfig::AvcConfig(AvcConfig {
-            width: track.width(),
-            height: track.height(),
-            seq_param_set: track.sequence_parameter_set()?.to_vec(),
-            pic_param_set: track.picture_parameter_set()?.to_vec(),
-        }),
-        MediaType::H265 => MediaConfig::HevcConfig(HevcConfig {
-            width: track.width(),
-            height: track.height(),
-        }),
-        MediaType::VP9 => MediaConfig::Vp9Config(Vp9Config {
-            width: track.width(),
-            height: track.height(),
-        }),
-        MediaType::AAC => MediaConfig::AacConfig(AacConfig {
-            bitrate: track.bitrate(),
-            profile: track.audio_profile()?,
-            freq_index: track.sample_freq_index()?,
-            chan_conf: track.channel_config()?,
-        }),
-        MediaType::TTXT => MediaConfig::TtxtConfig(TtxtConfig {}),
-    };
+struct NullNalReader {
+    decoder: RbspDecoder<NullNalHandler>,
+    start: u64,
+    push: u64,
+    end: u64,
+}
+impl NalReader for NullNalReader {
+    type Ctx = ();
 
-    Ok(media_type)
+    fn start(&mut self, _ctx: &mut Context<Self::Ctx>) {
+        self.start += 1;
+    }
+    fn push(&mut self, _ctx: &mut Context<Self::Ctx>, _buf: &[u8]) {
+        self.push += 1;
+    }
+    fn end(&mut self, _ctx: &mut Context<Self::Ctx>) {
+        self.end += 1;
+    }
+}
+
+fn main() -> Result<(), Error> {
+    let args: Vec<String> = env::args().collect();
+
+    let mut input_file = File::open(&args[1]).unwrap();
+    let mut output_file = File::create(&args[2]).expect("Unable to create file");   
+
+    let size = input_file.metadata()?.len() as usize;
+
+    let mut buf = vec![0; size];
+    input_file.read(&mut buf[..]).unwrap();
+    let mut ctx = Context::default();
+    let nal_handler = NullNalHandler {};
+    let nal_reader = NullNalReader {
+        decoder: RbspDecoder::new(nal_handler),
+        start: 0,
+        push: 0,
+        end: 0,
+    };
+    let mut annexb_reader = AnnexBReader::new(nal_reader);
+
+    annexb_reader.start(&mut ctx);
+    annexb_reader.push(&mut ctx, &buf[..]);
+    annexb_reader.end_units(&mut ctx);
+
+    output_file.write_all(&*buf)
 }
