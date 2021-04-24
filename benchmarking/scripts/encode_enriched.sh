d3f7fb845e996276faecffe16e2069c4be32b292
aegroto <aegroto@protonmail.com>
2021-04-24 19:17:00 +0200

    messages saved in files

--- benchmarking/scripts/encode_enriched.sh
+++ benchmarking/scripts/encode_enriched.sh
@@ -2,5 +2,5 @@
 for filename in ./videos/raw/*; do
     videoname=$(basename $filename)
     videoid=${videoname%.*}
-    ./bin/rav1e-lcmg -y $filename  --threads $(nproc) --output ./videos/av1-enriched/$videoid.ivf --hidden-string "test" > results/enrich_outputs/$videoid.txt
+    ./bin/rav1e-lcmg -y $filename  --threads $(nproc) --speed 10 --output ./videos/av1-enriched/$videoid.ivf --hidden-string $(cat ./messages/$videoid.txt) > results/enrich_outputs/$videoid.txt
 done
