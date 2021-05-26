aa3e32cfbc33b4264ada9a9268b9b69d387d2c29
aegroto <aegroto@protonmail.com>
2021-05-26 15:01:09 +0200

    updated decode script to adapt to new decoder output file path syntax

--- benchmarking/scripts/commands/decode_enriched.sh
+++ benchmarking/scripts/commands/decode_enriched.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-./bin/aomdec-lcmg ./videos/av1-enriched/$CASE_ID.ivf -o ./videos/decoded-enriched/$CASE_ID.y4m > results/decode_outputs/$CASE_ID.txt 2> results/full_logs/decode_enriched/$CASE_ID.log
+HIDDEN_MESSAGE_OUTPUT_FILE=results/decode_outputs/$CASE_ID.txt ./bin/aomdec-lcmg ./videos/av1-enriched/$CASE_ID.ivf -o ./videos/decoded-enriched/$CASE_ID.y4m 2> results/full_logs/decode_enriched/$CASE_ID.log
